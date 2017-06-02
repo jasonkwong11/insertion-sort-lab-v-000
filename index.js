bd1f9a594d838c336c99e591f6b3fda356c89315
Jason Kwong <jasonkwong11@gmail.com>
2017-06-02 17:01:24 -0400

    implement insertion sort

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+  var currentMin = array[0]
+  var currentIndex = 0
+  array.forEach((currentValue, index, array) => {
+    if(currentValue < currentMin) {
+      currentMin = currentValue;
+      currentIndex = index;
+    }
+  })
+  array.splice(currentIndex, 1)
+  return currentMin
 }
 
 function insertionSort(array){
-
+  let sorted = [];
+  while(array.length != 0){
+    sorted.push(findMinAndRemove(array))
+  }
+  return sorted;
 }
